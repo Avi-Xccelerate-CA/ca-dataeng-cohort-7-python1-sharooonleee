919a15ec50602825201ec9da93dc03c0b42c8ca3
sharooonleee <aps20acasec@gmail.com>
2024-07-07 19:57:01 +0800

    Week 0 Assessment by LEE HO YAN Sharon
    dose function

--- week0.py
+++ week0.py
@@ -1,18 +1,19 @@
-# week 0 problem 1
-# create a function that returns a list of (vitamins,injections) tuples for each attribute that they need.
-# Complete problem statement in README.mds
-
-
-# your input type should be a list,
-# output should also be a list containing tuples
-# Example:
-# input: [250, 0, 250, 0, 0, 0]
-# expected output: [(25,0),(0,0),(25,0),(0,0),(0,0),(0,0)]
-# input: [500, 1, 2, 3, 4, 5]
-#expected output: "No medicine given"
-# HINT: using % operator to find remainder may be helpful
 def dose(needs):
-    #YOUR SOLUTION STARTS HERE
+    output = []
+    vita = []
+    inj = []
+    for attribute in needs:
+        if 0 <= attribute < 250:
+            if attribute % 10 == 0:
+                vita.append(int(attribute / 10))
+                inj.append(0)
+            else:
+                vita.append(int(attribute // 10 + 1))
+                inj.append(int(vita[-1] * 10 - attribute))
+        elif attribute >= 250 or sum(attribute)>500:
+            return(("No medicine given"))
+    output = list(zip(vita, inj))
+    return output
 
-    #YOUR SOLUTION ENDS HERE
+
 
