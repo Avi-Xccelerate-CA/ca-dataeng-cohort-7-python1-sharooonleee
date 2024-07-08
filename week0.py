9d1c5d9d2c44073878b17a9465f744475549cb9c
sharooonleee <aps20acasec@gmail.com>
2024-07-08 11:00:19 +0800

    added criteria (if sum of list >500, return no medicine given

--- week0.py
+++ week0.py
@@ -1,19 +1,22 @@
 def dose(needs):
-    output = []
     vita = []
     inj = []
-    for attribute in needs:
-        if 0 <= attribute < 250:
-            if attribute % 10 == 0:
-                vita.append(int(attribute / 10))
-                inj.append(0)
-            else:
-                vita.append(int(attribute // 10 + 1))
-                inj.append(int(vita[-1] * 10 - attribute))
-        elif attribute >= 250 or sum(attribute)>500:
-            return(("No medicine given"))
-    output = list(zip(vita, inj))
-    return output
+    if sum(needs)>500:
+        return("No medicine given")
+    else:
+        for attribute in needs:
+            if 0 <= attribute < 250:
+                if attribute % 10 == 0:
+                    vita.append(int(attribute / 10))
+                    inj.append(0)
+                else:
+                    vita.append(int(attribute // 10 + 1))
+                    inj.append(int(vita[-1] * 10 - attribute))
+            elif attribute >= 250:
+                return(("No medicine given"))
+    output = list(zip(vita, inj)) 
+    return output 
+
 
 
 
